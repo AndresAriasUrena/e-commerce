e6d451731c2ab8ace11ffc2c61750479a06909f4
Andres Arias <andresarias@Andress-MacBook-Pro.local>
2024-02-22 23:07:43 -0600

    duplication fixed

--- src/app/(pages)/products/page.tsx
+++ src/app/(pages)/products/page.tsx
@@ -6,33 +6,37 @@ import { fetchDoc } from '../../_api/fetchDoc'
 import { fetchDocs } from '../../_api/fetchDocs'
 import { Blocks } from '../../_components/Blocks'
 import { Gutter } from '../../_components/Gutter'
+import { HR } from '../../_components/HR'
 import Filters from './Filters'
 
 import classes from './index.module.scss'
 
 const Products = async () => {
-  const {isEnabled: isDraftMode} = draftMode()
+  const { isEnabled: isDraftMode } = draftMode()
 
   let page: Page | null = null
   let categories: Category[] | null = null
 
   try {
     page = await fetchDoc<Page>({
-        collection: 'pages',
-        slug: 'products',
-        draft: isDraftMode,
+      collection: 'pages',
+      slug: 'products',
+      draft: isDraftMode,
     })
 
     categories = await fetchDocs<Category>('categories')
   } catch (error) {
     console.log(error)
   }
+
   return (
     <div className={classes.container}>
-        <Gutter className={classes.products}>
-            <Filters />
-            <Blocks blocks={page?.layout} disableTopPadding={true}/>
-        </Gutter>
+      <Gutter className={classes.products}>
+        <Filters />
+        <h2>Test</h2>
+        {/* <Blocks blocks={page?.layout} disableTopPadding={true} /> */}
+      </Gutter>
+      <HR />
     </div>
   )
 }
